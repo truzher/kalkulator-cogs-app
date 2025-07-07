d7490a77435411c2674e92a45a1a66364de5a302
truzher <78016709+truzher@users.noreply.github.com>
2025-07-08 04:21:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // === KONFIGURASI SUPABASE ===
     const SUPABASE_URL = 'https://ubfbsmhyshosiihaewis.supabase.co'; 
-    const SUPABASE_ANON_KEY = 'PASTE_KUNCI_ANON_LOE_YANG_BENAR_DI_SINI';
+    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViZmJzbWh5c2hvc2lpaGFld2lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4NzEwNjEsImV4cCI6MjA2NzQ0NzA2MX0.6mSpqn-jeS4Ix-2ZhBXFygPzxrQMQhCDzxyOgG5L9ss';
     const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
     // === VARIABEL GLOBAL ===
